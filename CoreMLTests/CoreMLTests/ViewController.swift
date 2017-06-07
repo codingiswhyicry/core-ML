ef9bf812db8e5021072979f60f40fe27f75080fa
Amanda Southworth <amanda.southworth@myds.us>
2017-06-07 09:14:46 -0700

    added CoreML text project

---
+++ CoreMLTests/CoreMLTests/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CoreMLTests
+//
+//  Created by Amanda Southworth on 6/7/17.
+//  Copyright © 2017 Amanda Southworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
